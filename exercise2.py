6ba87ed206537f71df9fc759e214e6b54a7407ee
Kath <kathybunao.com>
2020-03-07 22:05:55 +0800

    Celsius to Fahrenheit

---
+++ exercise2.py
@@ -0,0 +1,8 @@
+print("Celsius to Fahrenheit Converter")
+name = input("Name:")
+print("Hello", name,"!")
+
+ctemp = input("Input Celsius:")
+Celsius = float(ctemp)
+Fahrenheit = (float(ctemp)) * 9/5) + 32
+print ("Fahrenheit:", Fahrenheit)
